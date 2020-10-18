69e9778d3bb333ae1f9b9912302bdc70157e58a5
Josiah Powell <josiahpowell@outlook.com>
2020-10-17 21:34:34 -0500

    Adding Initial webmanifest

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "development",
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Allocated",
+      short_name: "Allocated - A budget app",
+      description: "An application for keeping track of finances",
+      background_color: "#ffccaa",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-192x192.png"),
+          sizes: [192, 512],
+          destination: path.join("assets", "icons")
+        }
+      ]
+    })
+  ]
+};
+module.exports = config;
